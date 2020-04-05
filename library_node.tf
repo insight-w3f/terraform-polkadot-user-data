e77789a886d527f71e996f0128e5c73c2fcad3f9
Richard Mah <dick.mah@gmail.com>
2020-04-05 16:15:06 -0400

    Update mount point

--- library_node.tf
+++ library_node.tf
@@ -1,10 +1,10 @@
 data "template_file" "library_node" {
   template = <<-EOT
     systemctl stop polkadot
-    mkdir /mnt/disks/nvme/polkadot
-    chown polkadot:polkadot /mnt/disks/nvme/polkadot
-    mv /home/polkadot/.local/share/polkadot/chains /mnt/disks/nvme/polkadot/
-    ln -s /mnt/disks/nvme/polkadot /home/polkadot/.local/share
+    mkdir /data/polkadot
+    chown polkadot:polkadot /data/polkadot
+    mv /home/polkadot/.local/share/polkadot/chains /data/polkadot/
+    ln -s /data/polkadot /home/polkadot/.local/share
     systemctl start polkadot
   EOT
 }
