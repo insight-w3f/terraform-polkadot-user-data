26eade6e24ca35bf121023b0e9b9059511e176b9
Richard Mah <dick.mah@gmail.com>
2020-04-28 11:57:41 -0400

    update consul

--- consul.tf
+++ consul.tf
@@ -1,48 +1,24 @@
 data "template_file" "consul" {
   template = <<-EOF
-apt install -y zip
-curl --silent --remote-name https://releases.hashicorp.com/consul/1.6.1/consul_1.6.1_linux_amd64.zip
-unzip consul_1.6.1_linux_amd64.zip
-chown root:root consul
-mv consul /usr/local/bin/
-useradd --system --home /etc/consul.d --shell /bin/false consul
-mkdir --parents /opt/consul
-chown --recursive consul:consul /opt/consul
+
+%{if var.cloud_provider == "gcp"}
+PRIVIP=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip" -H "Metadata-Flavor: Google")
+%{endif}
+
+%{if var.cloud_provider == "aws"}
 PRIVIP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4/)
+%{endif}
 
-tee -a /etc/systemd/system/consul.service << CONSULSVCEND
-[Unit]
-Requires=network-online.target
-After=network-online.target
-ConditionFileNotEmpty=/etc/consul.d/consul.hcl
-
-[Service]
-User=consul
-Group=consul
-ExecStart=/usr/local/bin/consul agent -config-file=/etc/consul.d/consul.hcl -retry-join="provider=aws tag_key=consul-servers tag_value=auto-join addr_type=private_v4"
-ExecReload=/bin/kill -HUP $MAINPID
-KillSignal=SIGINT
-TimeoutStopSec=5
-Restart=on-failure
-SyslogIdentifier=consul
-
-[Install]
-WantedBy=multi-user.target
-CONSULSVCEND
-
-mkdir --parents /etc/consul.d
-tee -a /etc/consul.d/consul.hcl << CONSULHCLEND
+tee -a /etc/consul.d/10bind.json << EOJ
 {
-"bind_addr": "$PRIVIP",
-"datacenter": "${var.region}",
-"data_dir": "/opt/consul",
-"server": false
-"retry_join": ["provider=aws tag_key=consul-servers tag_value=auto-join addr_type=private_v4"]
+"advertise_addr": "$PRIVIP",
+"advertise_addr_wan": "$PRIVIP",
+"bind_addr": "$PRIVIP"
 }
-CONSULHCLEND
+EOJ
 
-chown --recursive consul:consul /etc/consul.d
-chmod 640 /etc/consul.d/consul.hcl
+chown consul:bin /etc/consul.d/10bind.json
+chmod 644 /etc/consul.d/10bind.json
 
 systemctl enable consul
 systemctl start consul
