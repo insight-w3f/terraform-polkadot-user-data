b5b64191f98f71e83ad37ca381b610b9b05b944d
Richard Mah <dick.mah@gmail.com>
2020-04-29 12:07:48 -0400

    add proper consul wait

--- consul.tf
+++ consul.tf
@@ -2,18 +2,21 @@ data "template_file" "consul" {
   template = <<-EOF
 
 %{if var.cloud_provider == "gcp"}
+INSTANCE_ID=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/name" -H "Metadata-Flavor: Google")
 PRIVIP=$(curl "http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/ip" -H "Metadata-Flavor: Google")
 %{endif}
 
 %{if var.cloud_provider == "aws"}
+INSTANCE_ID=$(wget -q -O - http://169.254.169.254/latest/meta-data/instance-id || die \"wget instance-id has failed: $?\")
 PRIVIP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4/)
 %{endif}
 
 tee -a /etc/consul/consul.d/10bind.json << EOJ
 {
-"advertise_addr": "$PRIVIP",
+"advertise_addr": "$INSTANCE_ID",
 "advertise_addr_wan": "$PRIVIP",
 "bind_addr": "$PRIVIP"
+"host_name":
 }
 EOJ
 
@@ -24,7 +27,11 @@ systemctl enable consul
 systemctl start consul
 
 echo "Waiting for Consul to join..."
-sleep 60
+until [ $(curl -s http://localhost:8500/v1/health/node/${INSTANCE_ID} | jq -r '.[0] | .Status') == "passing" ]
+do
+  printf '.'
+  sleep 5
+done
 
 consul services register /home/ubuntu/host-node-exporter-payload.json
 consul services register /home/ubuntu/polkadot-client-node-exporter-payload.json
