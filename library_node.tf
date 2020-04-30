aab250ea6a4130ef18af6f2a593d7266acc05b3b
Richard Mah <dick.mah@gmail.com>
2020-04-30 18:03:56 -0400

    forgot a fi

--- library_node.tf
+++ library_node.tf
@@ -8,6 +8,7 @@ data "template_file" "library_node" {
       mv /home/polkadot/.local/share/polkadot/chains /data/polkadot/
     else
       mkdir -p /home/polkadot/.local/share/
+    fi
     ln -s /data/polkadot /home/polkadot/.local/share
     systemctl start polkadot
   EOT
