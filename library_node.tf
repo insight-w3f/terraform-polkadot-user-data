3fde4335d73d2339510f5e09c740381170aa56be
Richard Mah <dick.mah@gmail.com>
2020-04-30 18:01:04 -0400

    update library node script

--- library_node.tf
+++ library_node.tf
@@ -1,9 +1,13 @@
 data "template_file" "library_node" {
   template = <<-EOT
     systemctl stop polkadot
-    mkdir /data/polkadot
+    mkdir -p /data/polkadot
     chown polkadot:polkadot /data/polkadot
-    mv /home/polkadot/.local/share/polkadot/chains /data/polkadot/
+    if [[ -d /home/polkadot/.local/share/polkadot/chains ]]
+    then
+      mv /home/polkadot/.local/share/polkadot/chains /data/polkadot/
+    else
+      mkdir -p /home/polkadot/.local/share/
     ln -s /data/polkadot /home/polkadot/.local/share
     systemctl start polkadot
   EOT
